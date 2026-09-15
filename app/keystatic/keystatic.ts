9321a76af5c84a32da731e48d77d3f43e8deffc0
subaohe <sbh.t.memory.nobody@gmail.com>
2026-09-16 00:07:37 +0800

    Fix Keystatic admin loading

--- app/keystatic/keystatic.ts
+++ app/keystatic/keystatic.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import config from "../../keystatic.config";
 import { makePage } from "@keystatic/next/ui/app";
 
--- components/lazy-contact-float.tsx
+++ components/lazy-contact-float.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { usePathname } from "next/navigation";
 
-export const LazyContactFloat = dynamic(
+const ContactFloat = dynamic(
   () => import("@/components/contact-float").then((mod) => mod.ContactFloat),
   { ssr: false },
 );
+
+export function LazyContactFloat() {
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/keystatic")) return null;
+
+  return <ContactFloat />;
+}
