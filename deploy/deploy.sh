529eeb6c35f5b565b4961fb167ed42b6a0675f5d
subaohe <sbh.t.memory.nobody@gmail.com>
2026-09-15 15:35:02 +0800

    Connect CloudPanel deployment path

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-APP_DIR="${APP_DIR:-/var/www/bohol-vending}"
+APP_DIR="${APP_DIR:-/home/boholvending/htdocs/boholvending.com}"
 cd "$APP_DIR"
 
 git fetch origin main
--- deploy/ecosystem.config.cjs
+++ deploy/ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name: "bohol-vending",
     script: "node_modules/next/dist/bin/next",
     args: "start -p 3000",
-    cwd: "/var/www/bohol-vending",
+    cwd: "/home/boholvending/htdocs/boholvending.com",
     env: { NODE_ENV: "production", PORT: 3000 },
     instances: 1,
     exec_mode: "fork",
